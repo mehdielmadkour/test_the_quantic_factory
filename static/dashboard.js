ec1f47814897f4d6e4f454257892b5e708ebb3b0
mehdielmadkour <mehdielmadkour27@gmail.com>
2021-11-10 14:34:35 +0100

    affichage des données sur le dashboard avec chartjs

--- static/dashboard.js
+++ static/dashboard.js
@@ -12,5 +12,31 @@ $.ajax({
 });
 
 function display_approval_time_by_district(response) {
-    console.log(response)
+    
+    let data = {
+        labels: response.district,
+        datasets: [{
+            label: 'approval time',
+            backgroundColor: 'rgb(10, 10, 200)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: response.approval_time
+        }]
+    }
+
+    let config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+
+    let approval_time_by_district = new Chart(
+        document.getElementById('approval_time_by_district'),
+        config
+    )
 }
